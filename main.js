14806c40b3545822b7a7c075eede3101a89d0467
RishitM <72683716+RishitM@users.noreply.github.com>
2020-12-05 14:42:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var one ="This is ny first code homework!!";
+console.log(one);
+var First_Name="Rishit";
+var Last_Name="Mahajan";
+var Full_Name=First_Name.concat(Last_Name);
+console.log(Full_Name);
+var number_1=10
+var number_2=5
+var add_ans=(number_1 + number_2);
+console.log(add_ans);
+var sub_ans=(number_1 - number_2);
+console.log(sub_ans);
+var mul_ans=(number_1 * number_2);
+console.log(mul_ans);
+var div_ans=( number_1 / number_2);
+console.log(div_ans);
